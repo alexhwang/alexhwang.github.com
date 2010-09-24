a6c27bdf8bdd01861ce247f9100a3e7ec93b0708
Alex Hwang <aykhwang@gmail.com>
2010-09-24 16:43:46 -0700

    hi

--- Home_files/Home.js
+++ Home_files/Home.js
@@ -1,9 +1,9 @@
-// Created by iWeb 3.0.1 local-build-20100923
+// Created by iWeb 3.0.1 local-build-20100924
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Home_files/HomeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
