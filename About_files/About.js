95a26b280b4285873eaaeea3b05637c1f77449d1
Alex Hwang <aykhwang@gmail.com>
2010-09-24 18:26:50 -0700

    more awesomeness

---
+++ About_files/About.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.1 local-build-20100924
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('About_files/AboutMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
